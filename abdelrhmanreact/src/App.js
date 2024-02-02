daf90225c4d1cfb59304870740f0df3a1f2c65f1
Abdelrhman312 <abdomoataz135@gmail.com>
2024-02-02 20:23:00 +0200

    new version

---
+++ abdelrhmanreact/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from "./HomePage/Home"
+import Info from "./InfoPage/Info"
+
+const App = () => {
+  return (
+    <div>
+
+       <Home name="abdelrhmanmoataz"     />
+       <Info/>
+      
+       </div>
+  )
+}
+
+export default App
